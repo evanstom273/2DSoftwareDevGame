6d7d9470ebb363e26c4faafb86ad7e3135cc6458
evanstom273 <60609303+evanstom273@users.noreply.github.com>
2026-04-01 13:55:52 +0100

    ProductData v.2

--- Scripts/ProductData.gd
+++ Scripts/ProductData.gd
@@ -84,3 +84,38 @@ enum GameTags {
     LEVELING,
     UPGRADE_SYSTEM
 }
+enum OfficeSoftwareTypes {
+    NONE,
+
+    WORD_PROCESSOR,
+    SPREADSHEET,
+    PRESENTATION,
+    DATABASE,
+
+    EMAIL_CLIENT 
+}
+
+@export_group("Product Details")
+@export var product_name = str("")
+@export var product_type: ProductType = ProductType.NONE:
+    set(value):
+        product_type = value
+        notify_property_list_changed()
+@export var software_type: SoftwareType = SoftwareType.NONE:
+    set(value):
+        software_type = value
+        notify_property_list_changed()
+@export var game_genres: Array[GameGenres] = []
+@export var game_tags: Array[GameTags] = []
+@export var required_game_engine: ProductResource
+
+func _validate_property(property: Dictionary) -> void:
+    # Show only properties relevant to the selected product setup.
+    if property.name == "software_type" and product_type != ProductType.SOFTWARE:
+        property.usage = PROPERTY_USAGE_NO_EDITOR
+    if property.name == "game_genres" and software_type != SoftwareType.GAME:
+        property.usage = PROPERTY_USAGE_NO_EDITOR
+    if property.name == "game_tags" and software_type != SoftwareType.GAME:
+        property.usage = PROPERTY_USAGE_NO_EDITOR
+    if property.name == "required_game_engine" and software_type != SoftwareType.GAME:
+        property.usage = PROPERTY_USAGE_NO_EDITOR
